7f462f1b2cab14665c40b6e8235daeebd0a780e0
Ivan Malygin <ivan@swirldslabs.com>
2024-03-08 14:25:08 -0500

    Made `ParsedBucket` constructor public. Fixed ClassCastException Made `MerkleDb.TableMetadata` public. Made `MerkleDb.loadMetadata` public. Made `VirtualPipeline.pausePipelineAndExecute` public Made ParsedBucket.BucketEntry public for the validator Removed unused logger
    Signed-off-by: Ivan Malygin <ivan@swirldslabs.com>

--- platform-sdk/swirlds-merkledb/src/main/java/com/swirlds/merkledb/MerkleDb.java
+++ platform-sdk/swirlds-merkledb/src/main/java/com/swirlds/merkledb/MerkleDb.java
@@ -636,7 +636,7 @@ public final class MerkleDb {
      * @return List of loaded tables
      */
     @SuppressWarnings("rawtypes")
-    private static AtomicReferenceArray<TableMetadata> loadMetadata(final Path dir) {
+    public static AtomicReferenceArray<TableMetadata> loadMetadata(final Path dir) {
         final AtomicReferenceArray<TableMetadata> tableConfigs = new AtomicReferenceArray<>(MAX_TABLES);
         final Path tableConfigFile = dir.resolve(METADATA_FILENAME);
         if (Files.exists(tableConfigFile)) {
@@ -709,7 +709,7 @@ public final class MerkleDb {
      *
      */
     @SuppressWarnings("rawtypes")
-    private static class TableMetadata {
+    public static class TableMetadata {
 
         private final int tableId;
 
