b9ec7661e612710e493d0fa80303eb7832683226
Ivan Malygin <ivan@swirldslabs.com>
2025-03-13 11:54:48 -0400

    fix: 18169 Fixed MigrationTestingTool
    Signed-off-by: Ivan Malygin <ivan@swirldslabs.com>

--- platform-sdk/swirlds/build.gradle.kts
+++ platform-sdk/swirlds/build.gradle.kts
@@ -3,6 +3,7 @@ plugins { id("org.hiero.gradle.module.application") }
 
 mainModuleInfo {
     runtimeOnly("com.swirlds.platform.core")
+    runtimeOnly("com.swirlds.platform.core.test.fixtures")
     runtimeOnly("com.swirlds.merkle")
     runtimeOnly("com.swirlds.merkle.test.fixtures")
 }
