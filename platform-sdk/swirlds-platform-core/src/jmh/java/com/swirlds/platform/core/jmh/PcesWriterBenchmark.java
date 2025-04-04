177cb94b15d05f00cfa03918bc23b17e532de15c
mxtartaglia <maxi@swirldslabs.com>
2025-04-04 11:53:56 -0300

    chore: fix syncEveryEvent on PcesFileChannelWriter
    Signed-off-by: mxtartaglia <maxi@swirldslabs.com>

--- platform-sdk/swirlds-platform-core/src/jmh/java/com/swirlds/platform/core/jmh/PcesWriterBenchmark.java
+++ platform-sdk/swirlds-platform-core/src/jmh/java/com/swirlds/platform/core/jmh/PcesWriterBenchmark.java
@@ -1,17 +1,24 @@
 // SPDX-License-Identifier: Apache-2.0
 package com.swirlds.platform.core.jmh;
 
+import com.google.common.collect.ImmutableList;
 import com.swirlds.common.io.utility.FileUtils;
 import com.swirlds.common.test.fixtures.Randotron;
+import com.swirlds.common.test.fixtures.platform.TestPlatformContextBuilder;
+import com.swirlds.platform.event.preconsensus.FileSyncOption;
 import com.swirlds.platform.event.preconsensus.PcesFile;
 import com.swirlds.platform.event.preconsensus.PcesMutableFile;
-import com.swirlds.platform.test.fixtures.event.TestingEventBuilder;
+import com.swirlds.platform.test.fixtures.event.generator.StandardGraphGenerator;
+import com.swirlds.platform.test.fixtures.event.source.StandardEventSource;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import org.hiero.consensus.model.event.AncientMode;
 import org.hiero.consensus.model.event.PlatformEvent;
+import org.hiero.consensus.model.node.NodeId;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
@@ -28,34 +35,54 @@ import org.openjdk.jmh.annotations.Warmup;
 
 @State(Scope.Benchmark)
 @Fork(value = 1)
-@Warmup(iterations = 1, time = 1)
-@Measurement(iterations = 3, time = 10)
+@Warmup(iterations = 3, time = 1)
+@Measurement(iterations = 10, time = 10)
 public class PcesWriterBenchmark {
 
-    @Param({"true", "false"})
+    @Param({ "true", "false"})
     public boolean useFileChannelWriter;
-
-    @Param({"true", "false"})
-    public boolean syncEveryEvent;
-
-    private PlatformEvent event;
+    @Param({ "true", "false"})
+    public boolean useDsyncFlagWriter;
     private Path directory;
     private PcesMutableFile mutableFile;
+    static final List<PlatformEvent> EVENTS;
+    static final PlatformEvent EVENT;
+    static final NodeId selfId = NodeId.FIRST_NODE_ID;
+
+    static final int NUM_EVENTS = 100;
+
+    static {
+        final Randotron r = Randotron.create(0);
+        final StandardGraphGenerator generator = new StandardGraphGenerator(
+                TestPlatformContextBuilder.create().build(),
+                r.nextLong(),
+                new StandardEventSource(),
+                new StandardEventSource(),
+                new StandardEventSource(),
+                new StandardEventSource());
+
+        final var events = new ArrayList<PlatformEvent>();
+        for (int i = 0; i < NUM_EVENTS; i++) {
+            events.add(generator.generateEvent().getBaseEvent());
+        }
+        EVENTS = ImmutableList.copyOf(events);
+        EVENT=EVENTS.getFirst();
+        System.out.println("selfEvents:"
+                + EVENTS.stream()
+                        .filter(event -> event.getCreatorId().equals(selfId))
+                        .count());
+    }
 
     @Setup(Level.Iteration)
-    public void setup() throws IOException {
+    public void setup() throws IOException, InterruptedException {
+
+        Thread.sleep(10000);
         final Randotron r = Randotron.create(0);
 
-        event = new TestingEventBuilder(r)
-                .setAppTransactionCount(3)
-                .setSystemTransactionCount(1)
-                .setSelfParent(new TestingEventBuilder(r).build())
-                .setOtherParent(new TestingEventBuilder(r).build())
-                .build();
         directory = Files.createTempDirectory("PcesWriterBenchmark");
         final PcesFile file = PcesFile.of(AncientMode.GENERATION_THRESHOLD, r.nextInstant(), 1, 0, 100, 0, directory);
 
-        mutableFile = file.getMutableFile(useFileChannelWriter, syncEveryEvent);
+        mutableFile = file.getMutableFile(useFileChannelWriter, useDsyncFlagWriter);
     }
 
     @TearDown(Level.Iteration)
@@ -72,10 +99,40 @@ public class PcesWriterBenchmark {
     PcesWriterBenchmark.writeEvent             false                    true  thrpt    3  411138.079 ± 110692.138  ops/s
     PcesWriterBenchmark.writeEvent             false                   false  thrpt    3  643582.781 ± 154393.415  ops/s
     */
+
+    // Benchmark                       (syncEveryEvent)  (useFileChannelWriter)   Mode  Cnt        Score        Error
+    // Units
+    // PcesWriterBenchmark.writeEvent              true                    true  thrpt    3    25208.858 ±  99590.982
+    // ops/s
+    // PcesWriterBenchmark.writeEvent              true                   false  thrpt    3    34301.777 ±  94947.390
+    // ops/s
+    // PcesWriterBenchmark.writeEvent             false                    true  thrpt    3   513363.285 ± 447530.872
+    // ops/s
+    // PcesWriterBenchmark.writeEvent             false                   false  thrpt    3  1292696.028 ± 198412.982
+    // ops/s
+
+    // Benchmark                       (syncEveryEvent)  (useFileChannelWriter)   Mode  Cnt        Score        Error
+    // Units
+    // PcesWriterBenchmark.writeEvent              true                    true  thrpt    3   516916.172 ± 499773.057
+    // ops/s
+    // PcesWriterBenchmark.writeEvent              true                   false  thrpt    3    27711.788 ± 209413.534
+    // ops/s
+    // PcesWriterBenchmark.writeEvent             false                    true  thrpt    3   541710.710 ± 445208.701
+    // ops/s
+    // PcesWriterBenchmark.writeEvent             false                   false  thrpt    3  1356841.154 ± 978813.630
+    // ops/s
     @Benchmark
     @BenchmarkMode(Mode.Throughput)
     @OutputTimeUnit(TimeUnit.SECONDS)
     public void writeEvent() throws IOException {
-        mutableFile.writeEvent(event);
+            mutableFile.writeEvent(EVENT);
     }
+    @Benchmark
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    public void writeAndSyncEvent() throws IOException {
+            mutableFile.writeEvent(EVENT);
+            mutableFile.sync();
+    }
+
 }
---
+++ platform-sdk/swirlds-platform-core/src/testFixtures/java/com/swirlds/platform/test/fixtures/Experiment.java
@@ -0,0 +1,113 @@
+package com.swirlds.platform.test.fixtures;
+
+import com.google.common.collect.ImmutableList;
+import com.swirlds.common.io.utility.FileUtils;
+import com.swirlds.common.test.fixtures.Randotron;
+import com.swirlds.common.test.fixtures.platform.TestPlatformContextBuilder;
+import com.swirlds.platform.event.preconsensus.FileSyncOption;
+import com.swirlds.platform.event.preconsensus.PcesFile;
+import com.swirlds.platform.event.preconsensus.PcesMutableFile;
+import com.swirlds.platform.test.fixtures.event.TestingEventBuilder;
+import com.swirlds.platform.test.fixtures.event.generator.StandardGraphGenerator;
+import com.swirlds.platform.test.fixtures.event.source.StandardEventSource;
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import org.hiero.consensus.model.event.AncientMode;
+import org.hiero.consensus.model.event.PlatformEvent;
+import org.hiero.consensus.model.node.NodeId;
+
+public class Experiment {
+    private static final boolean SLEEP = true;
+    public static boolean useFileChannelWriter = true;
+
+    private static Path directory;
+    private static PcesMutableFile mutableFile;
+    static final List<PlatformEvent> EVENTS;
+    static final PlatformEvent event;
+    static final NodeId selfId = NodeId.FIRST_NODE_ID;
+
+    static final int NUM_EVENTS = 100;
+
+    static {
+        final Randotron r = Randotron.create(0);
+        EVENTS = List.of();
+        event = new TestingEventBuilder(r)
+                .setAppTransactionCount(3)
+                .setSystemTransactionCount(1)
+                .setSelfParent(new TestingEventBuilder(r).build())
+                .setOtherParent(new TestingEventBuilder(r).build())
+                .build();
+    }
+
+    private static List<PlatformEvent> extracted(final Randotron r) {
+        final StandardGraphGenerator generator = new StandardGraphGenerator(
+                TestPlatformContextBuilder.create().build(),
+                r.nextLong(),
+                new StandardEventSource(),
+                new StandardEventSource(),
+                new StandardEventSource(),
+                new StandardEventSource());
+
+        final var events = new ArrayList<PlatformEvent>();
+        for (int i = 0; i < NUM_EVENTS; i++) {
+            events.add(generator.generateEvent().getBaseEvent());
+        }
+        System.out.println("selfEvents:"
+                + EVENTS.stream()
+                .filter(event -> event.getCreatorId().equals(selfId))
+                .count());
+
+        return ImmutableList.copyOf(events);
+    }
+
+    public static void setup() throws IOException {
+
+        final Randotron r = Randotron.create(0);
+
+        directory = Files.createTempDirectory("PcesWriterBenchmark");
+        final PcesFile file = PcesFile.of(AncientMode.GENERATION_THRESHOLD, r.nextInstant(), 1, 0, 100, 0, directory);
+
+        mutableFile = file.getMutableFile(useFileChannelWriter, false);
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        System.out.println(ManagementFactory.getRuntimeMXBean().getName());
+        try {
+            setup();
+            System.out.println("one minute to start tracing");
+            extracted();
+            writeEvents();
+            System.out.println(LocalDateTime.now() + " stop tracing");
+            extracted();
+        } finally {
+            cleanup();
+        }
+    }
+
+    private static void extracted() throws InterruptedException {
+        if (SLEEP) {
+            Thread.sleep(50000);
+        }
+    }
+
+    public static void writeEvents() throws IOException {
+        final var start = System.currentTimeMillis();
+        System.out.println(LocalDateTime.now() + " write");
+        mutableFile.writeEvent(event);
+        System.out.println(LocalDateTime.now() + " sync");
+        mutableFile.sync();
+        System.out.println("elapsed time:" + (System.currentTimeMillis() - start));
+    }
+
+    public static void cleanup() throws IOException {
+        mutableFile.close();
+        FileUtils.deleteDirectory(directory);
+    }
+}
