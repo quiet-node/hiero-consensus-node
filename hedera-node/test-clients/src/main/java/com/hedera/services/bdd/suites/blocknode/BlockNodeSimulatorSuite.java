2e7c36d67cc22c1a8f7fff3e105490721f15b502
Derek Riley <derek.riley@swirldslabs.com>
2025-04-14 20:33:23 -0400

    more tests
    Signed-off-by: Derek Riley <derek.riley@swirldslabs.com>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/blocknode/BlockNodeSimulatorSuite.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/blocknode/BlockNodeSimulatorSuite.java
@@ -9,6 +9,7 @@ import static com.hedera.services.bdd.spec.utilops.UtilVerbs.doingContextual;
 import static com.hedera.services.bdd.spec.utilops.UtilVerbs.waitUntilNextBlock;
 import static java.time.temporal.ChronoUnit.SECONDS;
 
+import com.hedera.hapi.block.protoc.PublishStreamResponseCode;
 import com.hedera.services.bdd.HapiBlockNode;
 import com.hedera.services.bdd.HapiBlockNode.BlockNodeConfig;
 import com.hedera.services.bdd.HapiBlockNode.SubProcessNodeConfig;
@@ -97,7 +98,52 @@ public class BlockNodeSimulatorSuite {
                         blockNodeIds = {0},
                         simulatorPriorities = {0})
             })
-    final Stream<DynamicTest> node0StreamingBlockNodeConnectionDrops() {
+    final Stream<DynamicTest> node0StreamingBlockNodeConnectionDropsCanStreamGenesisBlock() {
+        AtomicReference<Instant> connectionDropTime = new AtomicReference<>();
+        AtomicReference<Long> lastVerifiedBlock = new AtomicReference<>();
+        return hapiTest(
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                doingContextual(spec -> connectionDropTime.set(Instant.now())),
+                blockNodeSimulator(0).shutDownImmediately(),
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                blockNodeSimulator(0).getLastVerifiedBlockExposing(lastVerifiedBlock::set),
+                blockNodeSimulator(0).startImmediately(),
+                assertHgcaaLogContainsTimeframe(
+                        NodeSelector.byNodeId(0),
+                        connectionDropTime::get,
+                        Duration.of(30, SECONDS),
+                        Duration.of(30, SECONDS),
+                        "Successfully reconnected to block node",
+                        "Received EndOfStream from block node",
+                        "PublishStreamResponseCode STREAM_ITEMS_BEHIND",
+                        "Ending stream and restarting at block " + lastVerifiedBlock.get()),
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                waitUntilNextBlock().withBackgroundTraffic(true));
+        // TODO Add log verification that the consensus node is able to stream from the genesis block through it's
+        // buffer
+    }
+
+    @HapiTest
+    @HapiBlockNode(
+            networkSize = 1,
+            blockNodeConfigs = {
+                @BlockNodeConfig(nodeId = 0, mode = BlockNodeMode.SIMULATOR),
+                @BlockNodeConfig(nodeId = 1, mode = BlockNodeMode.SIMULATOR)
+            },
+            subProcessNodeConfigs = {
+                @SubProcessNodeConfig(
+                        nodeId = 0,
+                        blockNodeIds = {0, 1},
+                        simulatorPriorities = {0, 1})
+            })
+    final Stream<DynamicTest> node0StreamingBlockNodeConnectionDropsSwitchLower() {
         AtomicReference<Instant> connectionDropTime = new AtomicReference<>();
         return hapiTest(
                 waitUntilNextBlock().withBackgroundTraffic(true),
@@ -106,13 +152,158 @@ public class BlockNodeSimulatorSuite {
                 doingContextual(spec -> connectionDropTime.set(Instant.now())),
                 blockNodeSimulator(0).shutDownImmediately(),
                 waitUntilNextBlock().withBackgroundTraffic(true),
+                // TODO assert that consensus node switches to lower priority block node
                 blockNodeSimulator(0).startImmediately(),
-                assertHgcaaLogContainsTimeframe(
-                        NodeSelector.byNodeId(0),
-                        connectionDropTime::get,
-                        Duration.of(30, SECONDS),
-                        Duration.of(30, SECONDS),
-                        "Successfully reconnected to block node"),
+                // TODO assert that consensus node switches back to higher priority block node
+                waitUntilNextBlock().withBackgroundTraffic(true));
+        // TODO Switching back, assert that the consensus node is able to stream from the appropriate block through it's
+        // buffer
+        //  currently the simulator always restarts from block 0.
+    }
+
+    @HapiTest
+    @HapiBlockNode(
+            networkSize = 1,
+            blockNodeConfigs = {
+                @BlockNodeConfig(nodeId = 0, mode = BlockNodeMode.SIMULATOR),
+                @BlockNodeConfig(nodeId = 1, mode = BlockNodeMode.SIMULATOR),
+                @BlockNodeConfig(nodeId = 2, mode = BlockNodeMode.SIMULATOR),
+                @BlockNodeConfig(nodeId = 3, mode = BlockNodeMode.SIMULATOR)
+            },
+            subProcessNodeConfigs = {
+                @SubProcessNodeConfig(
+                        nodeId = 0,
+                        blockNodeIds = {0, 1, 2, 3},
+                        simulatorPriorities = {0, 1, 2, 3})
+            })
+    final Stream<DynamicTest> node0StreamingBlockNodeConnectionDropsTrickle() {
+        AtomicReference<Instant> connectionDropTime = new AtomicReference<>();
+        return hapiTest(
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                doingContextual(spec -> connectionDropTime.set(Instant.now())),
+                blockNodeSimulator(0).shutDownImmediately(), // Pri 0
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                // TODO assert that consensus node switches to lower priority 1 block node
+                blockNodeSimulator(1).shutDownImmediately(), // Pri 1
+                // TODO assert that consensus node switches to lower priority 2 block node
+                blockNodeSimulator(2).shutDownImmediately(), // Pri 2
+                // TODO assert that consensus node switches to lower priority 3 block node
+                // Startup BN 1
+                blockNodeSimulator(1).startImmediately(),
+                // TODO assert that consensus node switches to higher priority 1 block node
+                // TODO assert that the consensus node doesn't retry connecting to Pri 2 anymore
+                blockNodeSimulator(0).startImmediately()
+                // CN Should switch to BN 1
+                // TODO assert that consensus node switches to higher priority 0 block node
+                );
+    }
+
+    @HapiTest
+    @HapiBlockNode(
+            networkSize = 1,
+            blockNodeConfigs = {
+                @BlockNodeConfig(nodeId = 0, mode = BlockNodeMode.SIMULATOR),
+            },
+            subProcessNodeConfigs = {
+                @SubProcessNodeConfig(
+                        nodeId = 0,
+                        blockNodeIds = {0},
+                        simulatorPriorities = {0})
+            })
+    final Stream<DynamicTest> node0StreamingBlockNodeMultipleEndOfStreamInSuccession() {
+        AtomicReference<Instant> connectionDropTime = new AtomicReference<>();
+        return hapiTest(
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                doingContextual(spec -> connectionDropTime.set(Instant.now())),
+                blockNodeSimulator(0).sendEndOfStreamImmediately(PublishStreamResponseCode.STREAM_ITEMS_INTERNAL_ERROR),
+                // TODO assert that the consensus node restarts stream
+                doingContextual(spec -> {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }),
+                blockNodeSimulator(0).sendEndOfStreamImmediately(PublishStreamResponseCode.STREAM_ITEMS_INTERNAL_ERROR),
+                // TODO assert that the consensus node restarts stream
+                doingContextual(spec -> {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }),
+                blockNodeSimulator(0).sendEndOfStreamImmediately(PublishStreamResponseCode.STREAM_ITEMS_INTERNAL_ERROR),
+                // TODO assert that the consensus node restarts stream
+                doingContextual(spec -> {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }),
+                waitUntilNextBlock().withBackgroundTraffic(true)
+                // TODO assert that the consensus node goes into exponential backoff
+                );
+    }
+
+    @HapiTest
+    @HapiBlockNode(
+            networkSize = 1,
+            blockNodeConfigs = {@BlockNodeConfig(nodeId = 0, mode = BlockNodeMode.SIMULATOR)},
+            subProcessNodeConfigs = {
+                @SubProcessNodeConfig(
+                        nodeId = 0,
+                        blockNodeIds = {0},
+                        simulatorPriorities = {0})
+            })
+    final Stream<DynamicTest> node0StreamingBlockNodeEndOfStreamResponseCodes() {
+        return hapiTest(
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                blockNodeSimulator(0).sendEndOfStreamImmediately(PublishStreamResponseCode.STREAM_ITEMS_UNKNOWN),
+                // TODO assert that the consensus node restarts stream
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                blockNodeSimulator(0).sendEndOfStreamImmediately(PublishStreamResponseCode.STREAM_ITEMS_TIMEOUT),
+                // TODO assert that the consensus node restarts stream
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                blockNodeSimulator(0).sendEndOfStreamImmediately(PublishStreamResponseCode.STREAM_ITEMS_OUT_OF_ORDER),
+                // TODO assert that the consensus node restarts stream
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                blockNodeSimulator(0)
+                        .sendEndOfStreamImmediately(PublishStreamResponseCode.STREAM_ITEMS_BAD_STATE_PROOF),
+                // TODO assert that the consensus node restarts stream
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                blockNodeSimulator(0).sendEndOfStreamImmediately(PublishStreamResponseCode.STREAM_ITEMS_BEHIND),
+                // TODO assert that the consensus node restarts stream
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                blockNodeSimulator(0).sendEndOfStreamImmediately(PublishStreamResponseCode.STREAM_ITEMS_INTERNAL_ERROR),
+                // TODO assert that the consensus node restarts stream
+                // this should immediately go into a longer backoff time period
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                blockNodeSimulator(0)
+                        .sendEndOfStreamImmediately(PublishStreamResponseCode.STREAM_ITEMS_PERSISTENCE_FAILED),
+                // TODO assert that the consensus node restarts stream
+                // this should immediately go into a longer backoff time period
+                waitUntilNextBlock().withBackgroundTraffic(true),
+                waitUntilNextBlock().withBackgroundTraffic(true),
                 waitUntilNextBlock().withBackgroundTraffic(true));
     }
 }
