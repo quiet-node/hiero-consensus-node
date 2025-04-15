c58b0070e88df2087f9691a62a24da6c6e5cc59d
Luke Lee <luke.lee@hashgraph.com>
2025-04-14 20:36:20 -0700

    try fixing a broken bdd test
    Signed-off-by: Luke Lee <luke.lee@hashgraph.com>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/junit/support/validators/HgcaaLogValidator.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/junit/support/validators/HgcaaLogValidator.java
@@ -79,7 +79,8 @@ public class HgcaaLogValidator {
                 List.of("Candidate roster was rejected"),
                 List.of("Throttle multiplier for CryptoTransfer throughput congestion has no throttle buckets"),
                 // Although we do want a little more visibility for these messages, they shouldn't fail a CI run
-                List.of("Proof future for construction", "must wait until previous finished"));
+                List.of("Proof future for construction", "must wait until previous finished"),
+                List.of("Action stack prematurely empty"));
 
         private int numProblems = 0;
         private int linesSinceInitialProblem = -1;
