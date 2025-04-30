83e748b7cbf2ded11bcd8fc48891634d18a89e79
Lazar Petrovic <lpetrovic05@gmail.com>
2025-04-30 15:43:17 -0700

    aaaaa
    Signed-off-by: Lazar Petrovic <lpetrovic05@gmail.com>

--- platform-sdk/swirlds-platform-core/src/testFixtures/java/com/swirlds/platform/test/fixtures/fxui/GuiEvent.java
+++ platform-sdk/swirlds-platform-core/src/testFixtures/java/com/swirlds/platform/test/fixtures/fxui/GuiEvent.java
@@ -1,7 +1,12 @@
 package com.swirlds.platform.test.fixtures.fxui;
 
+import com.hedera.hapi.platform.event.EventDescriptor;
 import com.hedera.pbj.runtime.io.buffer.Bytes;
+import com.swirlds.platform.internal.EventImpl;
+import java.util.Arrays;
 import java.util.List;
+import org.hiero.base.crypto.Hash;
+import org.hiero.consensus.model.event.EventDescriptorWrapper;
 
 public record GuiEvent(
         Bytes id,
@@ -15,4 +20,27 @@ public record GuiEvent(
         boolean judge,
         List<Bytes> stronglySeen
 ) {
+
+    public static GuiEvent fromEventImpl(final EventImpl eventImpl) {
+        return new GuiEvent(
+                eventImpl.getBaseHash().getBytes(),
+                eventImpl.getCreatorId().id(),
+                eventImpl.getBirthRound(),
+                eventImpl.getGeneration(),
+                eventImpl.getBaseEvent()
+                        .getAllParents()
+                        .stream()
+                        .map(EventDescriptorWrapper::eventDescriptor)
+                        .map(EventDescriptor::hash)
+                        .toList(),
+                eventImpl.getRoundCreated(),
+                eventImpl.isWitness(),
+                eventImpl.isFamous(),
+                eventImpl.isJudge(),
+                Arrays.stream(eventImpl.getStronglySeeP())
+                        .map(EventImpl::getBaseHash)
+                        .map(Hash::getBytes)
+                        .toList()
+        );
+    }
 }
