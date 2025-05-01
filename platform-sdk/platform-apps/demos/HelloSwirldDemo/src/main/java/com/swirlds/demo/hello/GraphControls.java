6e00de0cb56154efe5b485142abcde3cdcf83ca7
Artur Biesiadowski <artur.biesiadowski@swirldslabs.com>
2025-04-30 19:25:39 -0700

    WIP
    Signed-off-by: Artur Biesiadowski <artur.biesiadowski@swirldslabs.com>

--- platform-sdk/platform-apps/demos/HelloSwirldDemo/src/main/java/com/swirlds/demo/hello/GraphControls.java
+++ platform-sdk/platform-apps/demos/HelloSwirldDemo/src/main/java/com/swirlds/demo/hello/GraphControls.java
@@ -1,6 +1,8 @@
 package com.swirlds.demo.hello;
 
 import com.hedera.hapi.node.state.roster.RosterEntry;
+import java.util.Timer;
+import java.util.TimerTask;
 import javafx.application.Platform;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
@@ -9,12 +11,15 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 
 public class GraphControls {
 
     private GuiGraphGenerator graphGenerator;
     private GraphPane graphPane;
+    private Timer timer;
+    private TimerTask producer;
 
     public Pane setup(GuiGraphGenerator graphGenerator) {
         this.graphGenerator = graphGenerator;
@@ -27,11 +32,13 @@ public class GraphControls {
         vBox.getChildren().add(createBottomPane());
 
         HBox hBox = new HBox();
-        var leftPane = new Label("Left pane\n"+String.join(",",
+        var leftPane = new Label("Left pane\n" + String.join(",",
                 roster.rosterEntries().stream().map(e -> Long.toString(e.nodeId())).toList()));
         leftPane.setMinWidth(200);
         hBox.getChildren().add(leftPane);
         hBox.getChildren().add(vBox);
+
+        this.timer = new Timer(true);
         return hBox;
 
     }
@@ -43,18 +50,55 @@ public class GraphControls {
         hBox.getChildren().add(nextEvent);
 
         nextEvent.setOnAction(event -> {
-           var events = graphGenerator.generateEvents(1);
-           Platform.runLater(() -> {
-               for (GuiEvent guiEvent : events) {
-                   graphPane.addEventNode(guiEvent, (int) (Math.random()*500), (int) (Math.random()*500));
-               }
-           });
-
+            generateSingleEvent();
         });
+        var spacer = new Region();
+        spacer.setPrefWidth(50);
+        hBox.getChildren().add(spacer);
 
+        var pauseGenerator = new Button();
+        var runGenerator = new Button();
+
+        pauseGenerator.setGraphic(new ImageView("/pause.png"));
+        pauseGenerator.setOnAction(event -> {
+            if ( producer != null ) {
+                producer.cancel();
+                producer = null;
+                runGenerator.setDisable(false);
+                pauseGenerator.setDisable(true);
+            }
+        });
+        pauseGenerator.setDisable(true);
+        hBox.getChildren().add(pauseGenerator);
+
+        runGenerator.setGraphic(new ImageView("/play.png"));
+        runGenerator.setOnAction(event -> {
+            if (producer != null) {
+                return;
+            }
+            this.producer = new TimerTask() {
+                @Override
+                public void run() {
+                    generateSingleEvent();
+                }
+            };
+            timer.scheduleAtFixedRate(producer, 250, 250);
+            runGenerator.setDisable(true);
+            pauseGenerator.setDisable(false);
+        });
+        hBox.getChildren().add(runGenerator);
 
         return hBox;
 
+    }
+
+    private void generateSingleEvent() {
+        var events = graphGenerator.generateEvents(1);
+        Platform.runLater(() -> {
+            for (GuiEvent guiEvent : events) {
+                graphPane.addEventNode(guiEvent, (int) (Math.random() * 500), (int) (Math.random() * 500));
+            }
+        });
     }
 
 
