c5aafc9f99616ee384167f8720cf5fd0e1c2c06b
Nana Essilfie-Conduah <nana@swirldslabs.com>
2025-04-30 23:44:46 -0400

    Setup logic for y translation
    Signed-off-by: Nana Essilfie-Conduah <nana@swirldslabs.com>

--- platform-sdk/platform-apps/demos/HelloSwirldDemo/src/main/java/com/swirlds/demo/hello/GraphPane.java
+++ platform-sdk/platform-apps/demos/HelloSwirldDemo/src/main/java/com/swirlds/demo/hello/GraphPane.java
@@ -1,6 +1,7 @@
 package com.swirlds.demo.hello;
 
 import com.hedera.hapi.node.state.roster.Roster;
+import javafx.scene.Group;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
@@ -19,21 +20,23 @@ public class GraphPane extends Pane {
     private final int paneWidth = 900;
     private final int paneHeight = 900;
     private final int circleRadius = 20;
+    private Group nodeGroup;
+    public int maxEventHeight = 0; // note this is a negative value but visual the top
 
     public void setup(Roster roster) {
         this.setStyle("-fx-background-color: grey;");
         this.setPrefSize(paneWidth,paneHeight);
 
-        // add logic based on number of nodes
+        // add logic based on roster to set number of timelines
         setupTimeLines(roster);
-        // step 1 get roster size - use to determine number of vertical timelines
-        // step 2 get node id - use to determine horizontal position
-        // can use creator id and generation to determine horizontal position
+
+        nodeGroup = new Group();
+        this.getChildren().add(nodeGroup);
     }
 
     public void addEventNode(GuiEvent event) {
         final long nodePos = nodePositions.get(event.creator());
-        final long generationOffset = paneHeight - (event.generation() * circleRadius * 2) - (circleRadius * 2);
+        final long generationOffset = -(event.generation() * circleRadius * 2) - (circleRadius * 2);
 
         Sphere circle = new Sphere(circleRadius);
         var material = new PhongMaterial(event.color());
@@ -43,8 +46,23 @@ public class GraphPane extends Pane {
         circle.relocate(nodePos, generationOffset);
         Text text = new Text(event.generation() + " - " + event.creator());
         text.relocate(nodePos - 40, generationOffset + 10);
-        this.getChildren().addAll(circle, text);
+        nodeGroup.getChildren().addAll(circle, text);
         nodeViews.put(event.id(), event);
+
+        System.out.println("eventId: " + event.id().toString() + ", gen: " + event.generation() + ", nodePos: " + nodePos + ", generationOffset: " + generationOffset);
+
+        if (generationOffset < maxEventHeight) {
+            System.out.println("maxEventHeight: " + maxEventHeight + ", prefHeight: " + maxEventHeight + paneHeight);
+            maxEventHeight = (int) generationOffset;
+        }
+    }
+
+    public int getMaxEventHeight() {
+        return maxEventHeight;
+    }
+
+    public void setGroupYTranslation(int yTranslation) {
+        nodeGroup.setTranslateY(yTranslation);
     }
 
     private void setupTimeLines(Roster roster) {
