78eef359c8f5e418e457f3106219b03f841370a5
mxtartaglia <maxi@swirldslabs.com>
2025-05-01 00:36:05 -0300

    fix build gradle files and deps
    Signed-off-by: mxtartaglia <maxi@swirldslabs.com>

--- hedera-state-validator/build.gradle.kts
+++ hedera-state-validator/build.gradle.kts
@@ -1,10 +1,36 @@
 // SPDX-License-Identifier: Apache-2.0
 plugins {
     id("org.hiero.gradle.module.application")
+    id("org.hiero.gradle.feature.shadow")
 }
 
 dependencies{
-    compileOnly("org.projectlombok:lombok:1.18.30")
+    runtimeOnly("org.hyperledger.besu:besu-datatypes:24.3.3")
+    runtimeOnly("org.bouncycastle:bcprov-jdk18on:1.77")
+    runtimeOnly("org.bouncycastle:bcpkix-jdk18on:1.77")
+    implementation("com.google.code.gson:gson:2.10")
+    implementation("info.picocli:picocli:4.7.0")
+    implementation("com.goterl:lazysodium-java:5.1.1")
+    implementation("org.eclipse.collections:eclipse-collections:10.4.0")
+    implementation("io.github.classgraph:classgraph:4.8.65")
+    implementation("org.apache.logging.log4j:log4j-api:2.17.2")
+    implementation("org.apache.logging.log4j:log4j-core:2.17.2")
+    implementation("io.tmio:tuweni-bytes:2.4.2")
+    implementation("com.hedera.pbj:pbj-runtime:0.9.20")
+    implementation("com.google.protobuf:protobuf-java:3.17.3")
+    implementation("javax.inject:javax.inject:1")
+    implementation("com.google.dagger:dagger:2.51")
+    implementation("org.apache.commons:commons-lang3:3.14.0")
+    implementation("org.mockito:mockito-core:5.8.0")
+
+    // https://mvnrepository.com/artifact/io.prometheus/simpleclient_httpserver
+    implementation("io.prometheus:simpleclient_httpserver:0.16.0")
+    // https://mvnrepository.com/artifact/io.prometheus/simpleclient
+    implementation("io.prometheus:simpleclient:0.16.0")
+
+    // Mocking Bundle
+    implementation( "org.mockito:mockito-core:4.7.0")
+    implementation( "org.mockito:mockito-junit-jupiter:3.23.1")
     implementation("com.google.code.gson:gson:2.10")
 }
 
@@ -12,9 +38,7 @@ mainModuleInfo {
     annotationProcessor("com.swirlds.config.processor")
     annotationProcessor("com.google.auto.service.processor")
 
-    runtimeOnly("org.hyperledger.besu:besu-datatypes:24.3.3")
-    runtimeOnly("org.bouncycastle:bcprov-jdk18on:1.77")
-    runtimeOnly("org.bouncycastle:bcpkix-jdk18on:1.77")
+
     runtimeOnly("com.swirlds.config.impl")
     /*
      Traditional libraries that provide no module information at allfor example commons-cli:commons-cli:1.4.
@@ -23,23 +47,7 @@ mainModuleInfo {
     */
     // https://mvnrepository.com/artifact/org.javassist/javassist
 
-    requires("info.picocli:picocli:4.7.0")
-    requires("com.goterl:lazysodium-java:5.1.1")
-    requires("org.eclipse.collections:eclipse-collections:10.4.0")
-    requires("io.github.classgraph:classgraph:4.8.65")
-    requires("org.apache.logging.log4j:log4j-api:2.17.2")
-    requires("org.apache.logging.log4j:log4j-core:2.17.2")
-    requires("io.tmio:tuweni-bytes:2.4.2")
-    requires("com.hedera.pbj:pbj-runtime:0.9.20")
-    requires("com.google.protobuf:protobuf-java:3.17.3")
-    requires("javax.inject:javax.inject:1")
-    requires("com.google.dagger:dagger:2.51")
-    requires("org.apache.commons:commons-lang3:3.14.0")
-    requires("org.mockito:mockito-core:5.8.0")
-    requires("com.lmax:disruptor:3.4.4")
-    requires("io.github.artsok:rerunner-jupiter:2.1.6")
-    requires("com.hedera.cryptography:hedera-cryptography-pairings-api:0.2.0-SNAPSHOT")
-    requires("com.hedera.cryptography:hedera-cryptography-tss:0.2.0-SNAPSHOT")
+
     requires ("com.hedera.node.app.hapi.fees")
     requires ("com.hedera.node.app.hapi.utils")
     requires ("com.hedera.node.app.service.addressbook.impl")
@@ -70,11 +78,8 @@ mainModuleInfo {
     requires ("org.hiero.consensus.model")
     requires ("org.hiero.consensus.utility")
     requires ("com.hedera.pbj.runtime")
-    // https://mvnrepository.com/artifact/io.prometheus/simpleclient_httpserver
-    requires("io.prometheus:simpleclient_httpserver:0.16.0")
-    // https://mvnrepository.com/artifact/io.prometheus/simpleclient
-    requires("io.prometheus:simpleclient:0.16.0")
-    requires("io.github.java-diff-utils:java-diff-utils:4.12")
+    requires("io.github.javadiffutils")
+
 
 
     // Define the individual libraries
@@ -83,13 +88,11 @@ mainModuleInfo {
     requires("org.junit.jupiter.params")
     requires("org.mockito.junit.jupiter")
     requires("org.junit.platform.launcher")
-    // Mocking Bundle
-    requires( "org.mockito:mockito-core:4.7.0")
-    requires( "org.mockito:mockito-junit-jupiter:3.23.1")
 
     requires("com.swirlds.virtualmap")
     requires("info.picocli")
-    requires("com.google.code.gson:gson:2.10")
+    //requires("com.lmax.disruptor")
+    requires("artsok.rerunner.jupiter")
 
 }
 
