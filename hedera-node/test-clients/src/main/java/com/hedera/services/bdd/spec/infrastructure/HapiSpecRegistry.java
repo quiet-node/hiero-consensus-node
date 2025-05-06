c29a4460c7e30cdb55c4a4cd23c9d1c94a7b82fb
Neeharika Sompalli <52669918+Neeharika-Sompalli@users.noreply.github.com>
2025-05-06 17:29:57 -0500

    feat: Implement fee changes for  HIP-991 (#18991) (#19050)
    Signed-off-by: Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/spec/infrastructure/HapiSpecRegistry.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/spec/infrastructure/HapiSpecRegistry.java
@@ -428,6 +428,9 @@ public class HapiSpecRegistry {
         if (txn.hasMemo()) {
             meta.setMemo(txn.getMemo().getValue());
         }
+        if (txn.hasCustomFees()) {
+            meta.addAllCustomFees(txn.getCustomFees().getFeesList());
+        }
         put(name, meta.build());
         if (txn.hasExpirationTime()) {
             put(name, txn.getExpirationTime().getSeconds());
@@ -653,6 +656,10 @@ public class HapiSpecRegistry {
 
     public TopicID getTopicID(String name) {
         return get(name, TopicID.class);
+    }
+
+    public boolean hasTopicID(String name) {
+        return registry.containsKey(full(name, TopicID.class));
     }
 
     public TokenID getTokenID(String name) {
