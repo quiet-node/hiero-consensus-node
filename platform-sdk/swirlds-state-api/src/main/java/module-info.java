6a9e0e474e7ee625c8c810e80e45c1399d43a6bb
Nikita Lebedev <nikita.lebedev@limechain.tech>
2025-05-07 17:58:34 +0300

    spotless
    Signed-off-by: Nikita Lebedev <nikita.lebedev@limechain.tech>

--- platform-sdk/swirlds-state-api/src/main/java/module-info.java
+++ platform-sdk/swirlds-state-api/src/main/java/module-info.java
@@ -14,7 +14,7 @@ module com.swirlds.state.api {
     requires transitive com.swirlds.metrics.api;
     requires transitive org.hiero.base.crypto;
     requires transitive org.hiero.base.utility;
-    requires com.swirlds.logging;
     requires transitive org.apache.logging.log4j;
+    requires com.swirlds.logging;
     requires static transitive com.github.spotbugs.annotations;
 }
