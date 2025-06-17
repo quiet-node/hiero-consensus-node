51a88e18612df756f6d609648b59ead1fcda3639
Luke Lee <luke.lee@hashgraph.com>
2025-06-16 18:15:49 -0700

    fix hapi test
    Signed-off-by: Luke Lee <luke.lee@hashgraph.com>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/file/FileUpdateSuite.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/file/FileUpdateSuite.java
@@ -319,7 +319,7 @@ public class FileUpdateSuite {
                         .hasCostAnswerPrecheckFrom(MAX_GAS_LIMIT_EXCEEDED, BUSY));
     }
 
-    @LeakyHapiTest(overrides = {"contracts.maxKvPairs.individual", "contracts.maxKvPairs.aggregate"})
+    @LeakyHapiTest(overrides = {"contracts.maxKvPairs.aggregate"})
     final Stream<DynamicTest> kvLimitsEnforced() {
         final var contract = "User";
         final var gasToOffer = 1_000_000;
@@ -337,25 +337,23 @@ public class FileUpdateSuite {
                 contractCall(contract, INSERT_ABI, BigInteger.TWO, BigInteger.valueOf(4))
                         .payingWith(GENESIS)
                         .gas(gasToOffer),
-                /* And this one fails because 8 + 3 = 11 > 10 */
+                /* And this one succeeds as there is now no limit  */
                 contractCall(contract, INSERT_ABI, BigInteger.valueOf(3), BigInteger.valueOf(9))
                         .payingWith(GENESIS)
-                        .hasKnownStatus(MAX_CONTRACT_STORAGE_EXCEEDED)
+                        .hasKnownStatus(SUCCESS)
                         .gas(gasToOffer),
                 /* Confirm the storage size didn't change */
-                getContractInfo(contract).has(contractWith().numKvPairs(8)),
+                getContractInfo(contract).has(contractWith().numKvPairs(11)),
                 /* Now we update the per-contract limit to 1B mappings, but the aggregate limit to just 1 */
-                overridingTwo(
-                        "contracts.maxKvPairs.individual", "1000000000",
+                overriding(
                         "contracts.maxKvPairs.aggregate", "1"),
                 contractCall(contract, INSERT_ABI, BigInteger.valueOf(3), BigInteger.valueOf(9))
                         .payingWith(GENESIS)
                         .hasKnownStatus(MAX_STORAGE_IN_PRICE_REGIME_HAS_BEEN_USED)
                         .gas(gasToOffer),
-                getContractInfo(contract).has(contractWith().numKvPairs(8)),
+                getContractInfo(contract).has(contractWith().numKvPairs(11)),
                 /* Now raise the limits and confirm we can use more storage */
-                overridingTwo(
-                        "contracts.maxKvPairs.individual", "1000000000",
+                overriding(
                         "contracts.maxKvPairs.aggregate", "10000000000"),
                 contractCall(contract, INSERT_ABI, BigInteger.valueOf(3), BigInteger.valueOf(9))
                         .payingWith(GENESIS)
