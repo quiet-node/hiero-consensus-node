61e3733e319430d52c835f45a1b50af68311ed98
Stanimir Stoyanov <stanimir.stoyanov@limechain.tech>
2025-06-19 17:42:56 +0300

    fix: add module
    Signed-off-by: Stanimir Stoyanov <stanimir.stoyanov@limechain.tech>

--- hedera-node/hedera-smart-contract-service-impl/src/main/java/module-info.java
+++ hedera-node/hedera-smart-contract-service-impl/src/main/java/module-info.java
@@ -13,6 +13,7 @@ module com.hedera.node.app.service.contract.impl {
     requires transitive com.hedera.node.config;
     requires transitive com.hedera.node.hapi;
     requires transitive com.hedera.pbj.runtime;
+    requires transitive com.swirlds.common;
     requires transitive com.swirlds.config.api;
     requires transitive com.swirlds.metrics.api;
     requires transitive com.swirlds.state.api;
