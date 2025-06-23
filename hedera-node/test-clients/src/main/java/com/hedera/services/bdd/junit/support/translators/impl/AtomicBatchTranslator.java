541f745cb6f0f78fb57dca66b4cb4d935aae3f2a
Neeharika Sompalli <52669918+Neeharika-Sompalli@users.noreply.github.com>
2025-06-23 16:05:40 -0500

    test: Fixes `TrasactionRecordParityValidator` for AtomicBatch tests (#19811)
    Signed-off-by: Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/junit/support/translators/impl/AtomicBatchTranslator.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/junit/support/translators/impl/AtomicBatchTranslator.java
@@ -6,7 +6,9 @@ import static java.util.Objects.requireNonNull;
 
 import com.hedera.hapi.block.stream.output.StateChange;
 import com.hedera.hapi.block.stream.trace.TraceData;
+import com.hedera.hapi.node.transaction.ExchangeRateSet;
 import com.hedera.node.app.state.SingleTransactionRecord;
+import com.hedera.pbj.runtime.ParseException;
 import com.hedera.services.bdd.junit.support.translators.BaseTranslator;
 import com.hedera.services.bdd.junit.support.translators.BlockTransactionPartsTranslator;
 import com.hedera.services.bdd.junit.support.translators.inputs.BlockTransactionParts;
@@ -17,7 +19,7 @@ import java.util.List;
  * Translates a file update transaction into a {@link SingleTransactionRecord}, updating
  * {@link BaseTranslator} context when a special file is changed.
  */
-public class FileUpdateTranslator implements BlockTransactionPartsTranslator {
+public class AtomicBatchTranslator implements BlockTransactionPartsTranslator {
     public static final long EXCHANGE_RATES_FILE_NUM = 112L;
 
     @Override
@@ -43,12 +45,19 @@ public class FileUpdateTranslator implements BlockTransactionPartsTranslator {
                                         .mapUpdateOrThrow()
                                         .keyOrThrow()
                                         .fileIdKeyOrThrow();
-                                // when the fileUpdate transaction is inside an AtomicBatch don't set exchange rate
-                                // on the translated record
                                 if (fileId.fileNum() == EXCHANGE_RATES_FILE_NUM) {
-                                    baseTranslator.updateActiveRates(stateChange);
-                                    if (!parts.body().hasBatchKey()) {
-                                        receiptBuilder.exchangeRate(baseTranslator.activeRates());
+                                    final var contents = stateChange
+                                            .mapUpdateOrThrow()
+                                            .valueOrThrow()
+                                            .fileValueOrThrow()
+                                            .contents();
+                                    try {
+                                        // Set the active rates from state changes happened inside AtomicBatch
+                                        final var activeRates = ExchangeRateSet.PROTOBUF.parse(contents);
+                                        receiptBuilder.exchangeRate(activeRates);
+                                    } catch (ParseException e) {
+                                        throw new IllegalStateException(
+                                                "Rates file updated with unparseable contents", e);
                                     }
                                 }
                             }
