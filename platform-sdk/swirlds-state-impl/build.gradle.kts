88fd58edb7d8c291074e4f40a758fd345e4403d9
Ivan Malygin <ivan@swirldslabs.com>
2025-06-24 22:31:13 -0400

    Added missing dependency
    Signed-off-by: Ivan Malygin <ivan@swirldslabs.com>

--- platform-sdk/swirlds-state-impl/build.gradle.kts
+++ platform-sdk/swirlds-state-impl/build.gradle.kts
@@ -12,6 +12,7 @@ testModuleInfo {
     requires("com.swirlds.merkledb.test.fixtures")
     requires("com.swirlds.platform.core.test.fixtures")
     requires("com.swirlds.base.test.fixtures")
+    requires("com.swirlds.common.test.fixtures")
     requires("org.junit.jupiter.api")
     requires("org.junit.jupiter.params")
     requires("org.mockito")
