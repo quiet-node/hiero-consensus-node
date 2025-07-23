993466b18d80287e423a740063fcab1779b2a3e4
Tom Zheng <tom.zheng@swirldslabs.com>
2025-07-23 00:10:03 -0500

    updated the longevity workflow to run SDCT
    Signed-off-by: Tom Zheng <tom.zheng@swirldslabs.com>

--- .github/workflows/support/scripts/nightly.sh
+++ .github/workflows/support/scripts/nightly.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 #
-# Launch test client for nightly service performance test
+# Launch SDCT performance test
 Usage() {
     echo " "
     echo "Usage: <test-config>"
@@ -13,11 +13,11 @@ SERVER=${3}
 K8S_CLUSTER=${4}
 BUILD_TAG=${5}
 VERSION_SERVICE=${6}
-VERSION_MIRRORNODE=${7}
 
 USERPASSWORD="${USER}:${PASSWORD}"
 
 # File where web session cookie is saved
+#
 COOKIEJAR="$(mktemp)"
 CRUMB=$(curl --no-progress-meter -f -u "$USERPASSWORD" --cookie-jar "$COOKIEJAR" "$SERVER/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,%22:%22,//crumb)")
 
@@ -26,7 +26,7 @@ status=$?
 if [[ $status -eq 0 ]] ; then
 
 curl --no-progress-meter -f -X POST -u "$USERPASSWORD" --cookie "$COOKIEJAR" -H "$CRUMB" \
-    ${SERVER}/job/pipelines/job/nightly/buildWithParameters  \
+    ${SERVER}/job/pipelines/job/ops/buildWithParameters     \
     -F K8S_CLUSTER="${K8S_CLUSTER}"                          \
     -F BUILD_TAG="${BUILD_TAG}"                              \
     -F VERSION_SERVICE="${VERSION_SERVICE}"                  \
@@ -36,6 +36,11 @@ curl --no-progress-meter -f -X POST -u "$USERPASSWORD" --cookie "$COOKIEJAR" -H 
 fi
 rm "$COOKIEJAR"
 
-echo date "Nightly test started! - status [${status}]"
+if [[ $status -eq 0 ]] ; then
+    echo date "✅ Canonical Test started for [${BUILD_TAG}][${VERSION_SERVICE}]: ${status}"
+else
+    echo date "❌ Canonical Test failed to start for [${BUILD_TAG}][${VERSION_SERVICE}]: ${status}"
+fi
+
 exit $status
 
