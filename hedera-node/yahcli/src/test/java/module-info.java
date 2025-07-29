5dc26388f0dbb95150c2ae195732886b6337ecc9
Matt Hess <matt.hess@swirldslabs.com>
2025-07-28 23:09:37 -0600

    module info updates
    Signed-off-by: Matt Hess <matt.hess@swirldslabs.com>

--- hedera-node/yahcli/src/test/java/module-info.java
+++ hedera-node/yahcli/src/test/java/module-info.java
@@ -1,20 +1,8 @@
 // SPDX-License-Identifier: Apache-2.0
 module com.hedera.node.yahcli.test {
-    requires com.hedera.node.app;
-    requires com.hedera.node.hapi;
-    requires com.hedera.node.test.clients;
     requires com.hedera.node.yahcli;
-    requires org.hiero.base.utility;
-    requires com.github.spotbugs.annotations;
-    requires com.google.common;
-    requires com.google.protobuf;
-    requires info.picocli;
-    requires net.i2p.crypto.eddsa;
-    requires org.apache.logging.log4j;
     requires org.assertj.core;
-    requires org.junit.jupiter.api;
     requires org.junit.jupiter.params;
-    requires org.yaml.snakeyaml;
 
     // For test visibility
     opens com.hedera.services.yahcli.test to
