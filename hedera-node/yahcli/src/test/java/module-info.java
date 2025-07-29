884d5253fcf26008bbae100bbfdabaf0a7c30b9d
Matt Hess <matt.hess@swirldslabs.com>
2025-07-28 23:17:00 -0600

    module info updates
    Signed-off-by: Matt Hess <matt.hess@swirldslabs.com>

--- hedera-node/yahcli/src/test/java/module-info.java
+++ hedera-node/yahcli/src/test/java/module-info.java
@@ -3,6 +3,10 @@ module com.hedera.node.yahcli.test {
     requires com.hedera.node.yahcli;
     requires org.assertj.core;
     requires org.junit.jupiter.params;
+	requires info.picocli;
+	requires com.google.protobuf;
+	requires com.hedera.node.test.clients;
+	requires com.hedera.node.hapi;
 
     // For test visibility
     opens com.hedera.services.yahcli.test to
