b1d7b565467d912c15a52d85a006f2b80b1a4194
Ivan Malygin <ivan@swirldslabs.com>
2025-08-08 21:49:27 -0400

    feat: 20383 `export` command improvement (#20573)
    Signed-off-by: Ivan Malygin <ivan@swirldslabs.com>

--- hedera-state-validator/src/main/java/com/hedera/statevalidation/ExportCommand.java
+++ hedera-state-validator/src/main/java/com/hedera/statevalidation/ExportCommand.java
@@ -2,27 +2,42 @@
 package com.hedera.statevalidation;
 
 import com.hedera.statevalidation.exporters.JsonExporter;
+import com.hedera.statevalidation.exporters.SortedJsonExporter;
 import com.hedera.statevalidation.parameterresolver.StateResolver;
 import com.swirlds.platform.state.MerkleNodeState;
 import com.swirlds.platform.state.snapshot.DeserializedSignedState;
+import java.io.File;
 import java.io.IOException;
 import picocli.CommandLine;
 
 @CommandLine.Command(name = "export", description = "Exports the state")
 public class ExportCommand implements Runnable {
 
+    public static final int MAX_OBJ_PER_FILE = Integer.parseInt(System.getProperty("maxObjPerFile", "1000000"));
+    public static final boolean PRETTY_PRINT_ENABLED = Boolean.parseBoolean(System.getProperty("prettyPrint", "false"));
+
     @CommandLine.ParentCommand
     private StateOperatorCommand parent;
 
-    @CommandLine.Parameters(index = "0", arity = "0..1", description = "Service name")
+    @CommandLine.Parameters(index = "0", arity = "1", description = "Result directory")
+    private String resultDirStr;
+
+    @CommandLine.Parameters(index = "1", arity = "0..1", description = "Service name")
     private String serviceName;
 
-    @CommandLine.Parameters(index = "1", arity = "0..1", description = "State name")
+    @CommandLine.Parameters(index = "2", arity = "0..1", description = "State name")
     private String stateName;
 
     @Override
     public void run() {
         System.setProperty("state.dir", parent.getStateDir().getAbsolutePath());
+        File resultDir = new File(resultDirStr);
+        if (!resultDir.exists()) {
+            throw new RuntimeException(resultDir.getAbsolutePath() + " does not exist");
+        }
+        if (!resultDir.isDirectory()) {
+            throw new RuntimeException(resultDir.getAbsolutePath() + " is not a directory");
+        }
 
         MerkleNodeState state;
         try {
@@ -32,7 +47,13 @@ public class ExportCommand implements Runnable {
             throw new RuntimeException(e);
         }
 
-        final JsonExporter exporter = new JsonExporter(state, serviceName, stateName);
-        exporter.export();
+        boolean sorted = Boolean.parseBoolean(System.getProperty("sorted", "false"));
+        if (sorted) {
+            final SortedJsonExporter exporter = new SortedJsonExporter(resultDir, state, serviceName, stateName);
+            exporter.export();
+        } else {
+            final JsonExporter exporter = new JsonExporter(resultDir, state, serviceName, stateName);
+            exporter.export();
+        }
     }
 }
