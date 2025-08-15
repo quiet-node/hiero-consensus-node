e6323a3b2a4a0d671a59dfbf5d2b8a8c6db502fb
Alex Kuzmin <alex.kuzmin@swirldslabs.com>
2025-08-14 21:26:24 -0700

    debug
    Signed-off-by: Alex Kuzmin <alex.kuzmin@swirldslabs.com>

---
+++ .github/workflows/support/citr/startPodJava.sh
@@ -0,0 +1,38 @@
+node_id=$1
+isToClean=$2
+export MALLOC_ARENA_MAX=4
+
+APP_HOME=/opt/hgcapp/services-hedera/HapiApp2.0
+
+if [ "$isToClean" = "clean" ]
+then
+  echo "Cleaning old data ..."
+
+  cd $APP_HOME
+  rm -rf data/saved/*
+  rm -rf /opt/hgcapp/*Streams/*
+  rm -rf output/*
+  rm -rf /opt/hgcapp/services-hedera/HapiApp2.0/.archive
+
+  #cd $APP_HOME/data/keys
+  #bash generate.sh node1
+fi
+
+LANG=C.utf8
+JAVA_CLASS_PATH=data/lib/*:data/apps/*
+JAVA_OPTS="-XX:+UnlockExperimentalVMOptions -XX:+UseZGC -XX:ZAllocationSpikeTolerance=2 -XX:ConcGCThreads=14 -XX:ZMarkStackSpaceLimit=16g \
+-XX:MaxDirectMemorySize=64g \
+-XX:MetaspaceSize=100M -XX:+ZGenerational -Xlog:gc*:gc.log \
+--add-opens java.base/jdk.internal.misc=ALL-UNNAMED --add-opens java.base/java.nio=ALL-UNNAMED -Dio.netty.tryReflectionSetAccessible=true"
+JAVA_HOME=/usr/local/java
+USER=hedera
+HOME=/home/hedera
+JAVA_HEAP_MIN=32g
+JAVA_HEAP_MAX=118g
+JAVA_HEAP_OPTS="-Xms${JAVA_HEAP_MIN} -Xmx${JAVA_HEAP_MAX}"
+JAVA_MAIN_CLASS=com.hedera.node.app.ServicesMain
+LOGNAME=hedera
+PATH=/usr/local/java/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+cd $APP_HOME
+nohup /usr/bin/env java ${JAVA_HEAP_OPTS} ${JAVA_OPTS} -cp "${JAVA_CLASS_PATH}" "${JAVA_MAIN_CLASS}" -local ${node_id} > node.log 2>&1 &
