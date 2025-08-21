f10fa8c72e7280dcac90b275fc6d72b0803c8709
Michael Tinker <michael.tinker@swirldslabs.com>
2025-08-20 19:31:24 -0500

    Merge upstream
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- gradle/aggregation/build.gradle.kts
+++ gradle/aggregation/build.gradle.kts
@@ -22,7 +22,6 @@ dependencies {
     implementation(project(":test-clients"))
     implementation(project(":consensus-otter-docker-app"))
     implementation(project(":consensus-otter-tests"))
-    implementation(project(":yahcli"))
 }
 
 tasks.testCodeCoverageReport {
