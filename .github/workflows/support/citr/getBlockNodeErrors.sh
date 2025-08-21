ec6e60cdb5bfaaafa37faa4b9d1a6d1525eb2273
Alex Kuzmin <alex.kuzmin@swirldslabs.com>
2025-08-21 16:14:46 -0700

    NLG 0.6.2, blockNode for Longevity, too
    Signed-off-by: Alex Kuzmin <alex.kuzmin@swirldslabs.com>

--- .github/workflows/support/citr/getBlockNodeErrors.sh
+++ .github/workflows/support/citr/getBlockNodeErrors.sh
@@ -10,12 +10,12 @@ fi
 for i in `sh ${TOOLDIR}/kubectlt -n ${NAMESPACE} get pods | grep 'block-node-' | awk '{print $1}'`
 do
   sh ${TOOLDIR}/kubectlt -n ${NAMESPACE} exec -it ${i} -- \
-  bash -c "grep -h -i -E 'error|exception|warn' $BN_LOG/*.log" > podlog_${NAMESPACE}/${i}-errors.log
+  bash -c "grep -h -i -E 'error|exception|warn|severe' $BN_LOG/*.log" | grep -v error_prone_annotations > podlog_${NAMESPACE}/${i}-errors.log
 done
 
 cat podlog_${NAMESPACE}/*block-node*-errors.log | grep -v -E 'exception[\=]null' |\
 perl -ne 'if (/^\d{4}[\-]\d{2}[\-]\d{2}\s+\d{2}[\:]\d{2}[\:]\d{2}[\.]\d+\s+\d+\s+(.*)$/) {print "$1\n";} else {print;}' | perl -pne '~s/\d+/N/g' | sort | uniq -c | sort -n -k 1 -r |\
-grep -v 'Report[\[]' | grep -v 'contracts.evm.chargeGasOnEvmHandleException [\=] true' > podlog_${NAMESPACE}/error_summary_blocknodes.txt
+grep -v 'error_prone_annotations' > podlog_${NAMESPACE}/error_summary_blocknodes.txt
 log_
 
 for i in `sh ${TOOLDIR}/kubectlt -n ${NAMESPACE} get pods | grep 'block-node-' | awk '{print $1}'`
